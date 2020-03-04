df78c61800e31d3fb5980f1a42f1ea870dcc161e
SantiagoSR <santiago.santa.ramirez@gmail.com>
2020-03-04 15:43:29 -0500

    Luminosity project DJango

---
+++ luminosity/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Luminosity
+
+class LuminositySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Luminosity
+        fields = ('id', 'value')
+
