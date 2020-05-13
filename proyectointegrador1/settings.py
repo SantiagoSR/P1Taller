7fc95f135f4a6a7634bd6f6d1aa172805d871c6f
SantiagoSR <santiago.santa.ramirez@gmail.com>
2020-05-13 15:26:43 -0500

    Taller 3 terminado

--- proyectointegrador1/settings.py
+++ proyectointegrador1/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'proyectointegrador1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pi1-eafit-db',                     
+        'USER': 'ssantacrur@pi-eafit-db',                     
+        'PASSWORD': 'rynWJA37*',                  
+        'HOST': 'pi-eafit-db.mysql.database.azure.com',                     
+        'PORT': '3306',
     }
 }
 
